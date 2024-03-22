9dd41e83dfc4d4780318ca03164af7c0a4fa7f9f
Edward <10942547@uvu.edu>
2024-03-22 16:59:29 -0600

    implement basic form-handling

--- webApp/includes/formhandler.php
+++ webApp/includes/formhandler.php
@@ -1,2 +1,34 @@
 <?php
-	
+//var_dump($_SERVER["REQUEST_METHOD"]);
+	//this should be private
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	// vulnerable to cross-site-scripting without htmlspecialchars
+		$firstname = htmlspecialchars($_POST["firstname"]);
+		$lastname = htmlspecialchars($_POST["lastname"]);
+		$areacode = htmlspecialchars($_POST["areacode"]);
+		$phone = htmlspecialchars($_POST["phone"]);
+		$email = htmlspecialchars($_POST["email"]);
+		$total = htmlspecialchars($_POST["total"]);
+		if (empty($firstname)) {
+			exit();
+			header("Location: ../index.php");
+		}
+		echo "<br>";
+		echo $firstname;
+		echo "<br>";
+		echo $lastname;
+		echo "<br>";
+		echo $areacode;
+		echo "<br>";
+		echo $phone;
+		echo "<br>";
+		echo $email;
+		echo "<br>";
+		echo $total;
+		echo "<br>";
+		
+		header("Location: ../index.php");
+	}
+	else {
+		header("Location: ../index.php");
+	}
