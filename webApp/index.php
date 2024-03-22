94c2951ab4ff3dcf5caf9fca3a2dfd8bd22ca904
Edward <10942547@uvu.edu>
2024-03-22 15:09:26 -0600

    testing git hooks

---
+++ webApp/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Hello World</title>
+        <meta name="description" content="Our first page">
+        <meta name="keywords" content="html tutorial template">
+    </head>
+    <body>
+	<?php echo '<h1>Hello World!</h1>'; ?>
+	<main>
+		<form action="includes/formhanlder.php" method="post">
+			<label for="firstname">First Name:</label>
+			<input type="text" id="firstname" name="firstname" placeholder="Firstname" required>
+        		<br><br>
+		
+			<label for="lastname">Last Name:</label>
+        		<input type="text" id="lastname" name="lastname" placeholder="Lastname" required>
+        		<br><br>
+
+        		<button type="submit">Submit</button>
+		</form>
+
+	</main>
+    </body>
+</html>
+
