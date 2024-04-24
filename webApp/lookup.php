8be29234cdf480d50d21ac77bdfc3b4148cc1c2f
Edward <10942547@uvu.edu>
2024-04-24 12:53:51 -0600

    add lookup.php

---
+++ webApp/lookup.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Edward Haymore</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, inital-scale=1.0, minimum-scale=1.0, user-scalable=no">
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+		<script src="js/jquery-3.5.1.min.js"></script>
+		<script src="js/bootstrap.bundle.js"></script>
+		<script src="js/script.js"></script>
+	</head>
+		<?php
+//		require_once "config.php";
+		session_start();
+		echo $_SESSION["username"];
+		?>
+	<!-- NOTE: the required attribute is not enough to stop the user from submitting an empty form! user can still submit the form by using the inspect tool-->
+	<!-- Frontend stuff does not provide security! -->
+	<body>
+		<nav class="navbar navbar-dark navbar-expand-md p-md-3">
+			<a href="index.php"class="navbar-brand text-muted">Edward Haymore</a>
+			<button class="navbar-toggler" data-target="#nav-menu" data-toggle="collapse"><span class="navbar-toggler-icon"></span></button>
+			<div id="nav-menu" class="collapse navbar-collapse justify-content-center">
+			</div>
+		</nav>
+		<main class="jumbotron shadow-lg">
+		<h4 class= "text-decoration-none">Search Customers by Last Name</h4>
+		<form action="includes/lookup_formhandler.php" method="post">
+		<div class="form-group">
+        		<input type="text" id="lastname" name="lastname" placeholder="Lastname" required>
+        </div>
+		</form>
+		</main>
+		
+	</body>
+</html>
